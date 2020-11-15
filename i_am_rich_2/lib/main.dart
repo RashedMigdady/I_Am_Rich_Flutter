e28b8d9bb93fd9d48d1c730e047b6bca5e9928b7
RashedMigdady <migdadyRashed@gmail.com>
2020-11-15 04:01:20 +0200

    3rd commit

--- i_am_rich_2/lib/main.dart
+++ i_am_rich_2/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Center(
-            child: Text("I Am Rich ^_^"),
+            child: Text("I Am Rich"),
           ),
           backgroundColor: Colors.blueGrey,
         ),
@@ -17,6 +22,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
